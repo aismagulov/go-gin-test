6f65b680c852c13af3687f817a43ad97edce8bf1
Arsen Ismagulov <arsen.ismagulov@gmail.com>
2023-07-11 22:24:30 +0200

    added redis store and docker-compose

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"aismagulov/go-gin-test/controller"
 	"aismagulov/go-gin-test/middleware"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,7 +16,16 @@ func main() {
 	router := gin.Default()
 
 	var secret string = os.Getenv("SESSION_SECRET")
-	store := cookie.NewStore([]byte(secret))
+	var redispass string = os.Getenv("REDIS_PASSWORD")
+	// store := cookie.NewStore([]byte(secret))
+	log.Println("Connecting to redis")
+	store, error := redis.NewStore(10, "tcp", "localhost:6379", redispass, []byte(secret))
+	//store, error := redis.NewStore(10, "tcp", "go-gin-test_redis-session_1:6379", "", []byte(secret))
+	if error != nil {
+		log.Println("Cannot connect to redis")
+		panic(error)
+	}
+
 	router.Use(sessions.Sessions("mysession", store))
 	router.POST("/login", controller.Login)
 	router.GET("/logout", controller.Logout)
